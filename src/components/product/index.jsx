2c7e5bb36d14979c25107f398d7ee1c29d33bb56
Aida Hashemi <aidahashemiam@gmail.com>
2024-09-07 22:41:03 +0330

    Add addToCart : just its place in product details page

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { CardContext } from "../../context/CardContext";
 import StarRating from "../../functions/StarRating";
 
+import styles from "./styles.module.css";
 const Product = () => {
   const { id } = useParams();
   const { cards, loading } = useContext(CardContext);
@@ -11,24 +12,32 @@ const Product = () => {
 
   //   const card = cards.find((card) => card.id === parseInt(id));
   const card = cards.find((card) => card.id === id);
-  //   if (card) alert("yes");
   if (!card) {
     return <div>Product not found</div>;
   }
 
   return (
-    <div>
-      <h1>{card.title}</h1>
-      <img src={card.url} alt="product" />
-      <p>{card.description}</p>
-      <p>Price: {card.price}</p>
-      <p>Discount: {card.discount}</p>
-      <StarRating
-        maxRating={5}
-        color="#fcc419"
-        size={24}
-        defaultRating={Math.floor(card.rate)}
-      />
+    <div className={styles.productDetails}>
+      <div className={styles.imgContainer}>
+        <img src={card.url} alt="product" />
+      </div>
+      {/* TODO productInfo is not completed yet  */}
+      <div className={styles.productInfo}>
+        <h1>{card.title}</h1>
+        <p>{card.description}</p>
+        <a href="#">visit the {card.title} Store</a>
+        <StarRating
+          maxRating={5}
+          color="#fcc419"
+          size={24}
+          defaultRating={Math.floor(card.rate)}
+        />
+        <p>Price: {card.price}</p>
+        <p>Discount: {card.discount}</p>
+      </div>
+      <div className={styles.addToCart}>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
     </div>
   );
 };
