a051b4393bcac203e55d3bc81693c4948ca45208
Aida Hashemi <aidahashemiam@gmail.com>
2024-09-11 01:09:49 +0330

    Complete Sidebarmenu

---
+++ src/components/menu/sidebarMenu/index.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import styles from "./styles.module.css";
+import { FaUserCircle } from "react-icons/fa";
+import { IoIosArrowForward, IoMdArrowBack } from "react-icons/io";
+import usFlag from "../../../assets/svg/usa.svg";
+import { TfiWorld } from "react-icons/tfi";
+
+const SidebarMenu = ({ isOpen, onClose }) => {
+  const [currentMenu, setCurrentMenu] = useState("main");
+
+  const renderMainMenu = () => (
+    <div className={styles.digitalContent}>
+      <MenuSection title="Digital Content & Devices">
+        <MenuItem onClick={() => setCurrentMenu("amazonMusic")}>
+          Amazon Music
+        </MenuItem>
+        <MenuItem>Kindle E-readers & Books</MenuItem>
+        <MenuItem>Amazon Appstore</MenuItem>
+      </MenuSection>
+
+      <hr />
+
+      <MenuSection title="Shop by Department">
+        <MenuItem>Electronics</MenuItem>
+        <MenuItem>Computer</MenuItem>
+        <MenuItem>Smart Home</MenuItem>
+        <MenuItem>Arts & Crafts</MenuItem>
+        <MenuItem>See all</MenuItem>
+      </MenuSection>
+
+      <hr />
+
+      <MenuSection title="Programs & Features">
+        <MenuItem>Gift Cards</MenuItem>
+        <MenuItem>Shop By Interest</MenuItem>
+        <MenuItem>Amazon live</MenuItem>
+        <MenuItem>International Shopping</MenuItem>
+        <MenuItem>See all</MenuItem>
+      </MenuSection>
+
+      <hr />
+
+      <MenuSection title="Help & Settings">
+        <MenuItem>
+          <span>Your Account</span>
+        </MenuItem>
+        <MenuItem>
+          <div className={styles.hasIcon}>
+            <TfiWorld />
+            <span>English</span>
+          </div>
+        </MenuItem>
+        <MenuItem>
+          <div className={styles.hasIcon}>
+            <img alt="usFlag" src={usFlag} />
+            <span>United States</span>
+          </div>
+        </MenuItem>
+        <MenuItem>Customer Service</MenuItem>
+        <MenuItem>Sign in</MenuItem>
+      </MenuSection>
+    </div>
+  );
+
+  const renderAmazonMusicMenu = () => (
+    <div className={styles.amazonMusic}>
+      <p onClick={() => setCurrentMenu("main")}>
+        <IoMdArrowBack />
+        <span>Main Menu</span>
+      </p>
+      <h3>Stream Music</h3>
+      <ul>
+        <li>Amazon Music Unlimited</li>
+        <li>Free Streaming Music</li>
+        <li>Podcasts</li>
+        <li>Open Web Player</li>
+        <li>Download the app</li>
+      </ul>
+    </div>
+  );
+
+  return (
+    <div
+      className={`${styles.sidebarMenu} ${isOpen ? styles.open : ""}`}
+      onClick={onClose}
+    >
+      <div
+        id="container"
+        className={styles.sidebarContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <header>
+          <button className={styles.closeButton} onClick={onClose}>
+            &times;
+          </button>
+          <FaUserCircle />
+          <p>Hello, sign in</p>
+        </header>
+        <main>
+          {currentMenu === "main" ? renderMainMenu() : renderAmazonMusicMenu()}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+const MenuSection = ({ title, children }) => (
+  <div>
+    <h3>{title}</h3>
+    <ul>{children}</ul>
+  </div>
+);
+
+const MenuItem = ({ onClick, children }) => (
+  <li onClick={onClick}>
+    {children}
+    <IoIosArrowForward />
+  </li>
+);
+
+export default SidebarMenu;
--- src/components/menu/topbarMenu/index.jsx
+++ src/components/menu/topbarMenu/index.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
+import SidebarMenu from "../sidebarMenu";
 import styles from "./styles.module.css";
 
 // TODO add this part to header
 const TopbarMenu = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <nav className={styles.topbarMenu}>
-      <ul>
-        <li>
-          <IoMdMenu size={24} />
-          All
-        </li>
-        <li>Todas&apos;s Deal</li>
-        <li>Customer Servise</li>
-        <li>Registy</li>
-        <li>Gift Cards</li>
-        <li>Sell</li>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.topbarMenu}>
+        <ul>
+          <li onClick={toggleSidebar}>
+            <IoMdMenu size={24} />
+            All
+          </li>
+          <li>Toda Deal</li>
+          <li>Customer Service</li>
+          <li>Registry</li>
+          <li>Gift Cards</li>
+          <li>Sell</li>
+        </ul>
+      </nav>
+      <SidebarMenu isOpen={isSidebarOpen} onClose={closeSidebar} />
+    </>
   );
 };
 
